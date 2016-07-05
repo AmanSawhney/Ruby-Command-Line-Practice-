185d5297a24b78187b20426dd7eb8ce17ecc5923
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-05 14:34:08 -0400

    finished phonebook homework

--- RubyProjects/hashpractice.rb
+++ RubyProjects/hashpractice.rb
@@ -1,10 +1,20 @@
-my_details = {'name' => 'Aman', 'favColor' => 'red'}
+my_details = {
+	'name' => 'Aman', 
+	'favColor' => 'red'
+}
 puts my_details["favColor"] #gievs back the key for favcolor
 my_details['girlFriend'] = "Rachel" #adds a key value pair to the hash
 puts my_details #prints the hash
 my_details.delete('favColor') #removes a key and value from the hash 
 puts my_details
-numberHash = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 10}  #creates hash with symbols and colons instead of with strings 
+numberHash = { #creates hash with symbols and colons instead of with strings 
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 10
+}  
 numberHash.each {|k, v| puts v } #goes through the array and prints the value 
 numberHash.each { |k,v| numberHash.delete(k) if v > 3} #deletes every value above 3
 puts numberHash #prints array
