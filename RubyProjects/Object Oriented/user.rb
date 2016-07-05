f7b5f4ff5b5d2e7f0f57891a4409afedd7b592a4
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-05 15:43:34 -0400

    Played with subclasses

--- RubyProjects/Object Oriented/user.rb
+++ RubyProjects/Object Oriented/user.rb
@@ -1,6 +1,8 @@
 class User
-	def initialize(name)
+	attr_accessor :name, :email
+	def initialize(name, email)
 		@name = name 
+		@email = email
 	end
 	def run
 		puts "Hey is your refrigerator running, well you better go catch it!"
@@ -11,12 +13,39 @@ class User
 	def set_name(name)
 		@name = name
 	end
+	def self.identify_yourself
+		puts "I am a class method"
+	end
 end
 
-user = User.new("Aman")
-user1 = User.new("Rachel")
+class Buyer < User 
+	def run 
+		puts "Sell me some refrigerators it isnt running how I think it should man"
+	end
+end 
+
+class Seller < User 
+
+end
+
+class Admin < User 
+
+end
+
+user = User.new("Aman", "Aman@example.com")
+user1 = User.new("Rachel", "Rachel@example.com")
 
 puts user.get_name
 user.set_name("Ethan")
 puts user.get_name
 user1.run
+
+buyer1 = Buyer.new("buyer", "buyer@example.com")
+buyer1.run
+seller1 = Seller.new("seller", "seller@example.com")
+seller1.run
+admin1 = Admin.new("admin", "admin@example.com")
+admin1.run
+
+
+User.identify_yourself
