e6414b838b150ad6f7f83d5eefa64920a3e96100
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-05 11:09:42 -0400

    Played with strings and learned how to use input

---
+++ RubyProjects/playground.rb
@@ -0,0 +1,6 @@
+puts "Hello enter your first name"
+
+first_name = gets.chomp ##Gets input from terminal 
+puts "enter your last name"
+last_name = gets.chomp
+puts "Welcome #{first_name} #{last_name}, to the playground!"
