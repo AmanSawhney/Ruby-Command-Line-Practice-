97de990a7273c4e42285c4c94414874493b0dc3d
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-05 14:02:42 -0400

    added select method to hash pratice

--- RubyProjects/hashpractice.rb
+++ RubyProjects/hashpractice.rb
@@ -5,4 +5,7 @@ puts my_details #prints the hash
 my_details.delete('favColor') #removes a key and value from the hash 
 puts my_details
 numberHash = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 10}  #creates hash with symbols and colons instead of with strings 
-numberHash.each {|k, v| puts v #goes through the array and prints the value 
+numberHash.each {|k, v| puts v } #goes through the array and prints the value 
+numberHash.each { |k,v| numberHash.delete(k) if v > 3} #deletes every value above 3
+puts numberHash #prints array
+numberHash.select {|k,v| puts v if v.odd?} #prints odd values
