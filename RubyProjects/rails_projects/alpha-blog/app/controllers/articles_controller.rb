c91f887df27e2aff7d52f03aacee53f03003aa48
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-06 14:25:20 -0400

    Made a display for the articles

--- RubyProjects/rails_projects/alpha-blog/app/controllers/articles_controller.rb
+++ RubyProjects/rails_projects/alpha-blog/app/controllers/articles_controller.rb
@@ -3,10 +3,18 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end 
 	def create 
-		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+	    @article = Article.new(article_params)
+		if @article.save 
+			flash[:notice] = "Article was created"
+			redirect_to article_path(@article)
+		else
+			render :new #validations failed
+		end
 	end
+	def show
+		@article = Article.find(params[:id])
+	end
+
 
 	private
 		def article_params 
