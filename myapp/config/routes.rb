b1508fb9ad5cdf550d2e2b04f4d7de49c94b89d0
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-06 15:22:39 -0400

    Added the ability to view all articles

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'welcome/home', to: 'welcome#home'
-  get 'about/me', to: 'about#me'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
